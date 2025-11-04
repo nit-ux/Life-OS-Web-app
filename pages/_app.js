f73172db4d04f94c4f8f16dafb34aca0b2f366cc
nit-ux <niteshnaagodiyaofficial@gmail.com>
2025-11-04 22:59:47 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,12 @@ import { useRouter } from 'next/router';
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // Don't show layout on the login page
+  // If it's the login page (index), don't use the layout.
   if (router.pathname === '/') {
-    // We will need to move the dashboard to a new route like /dashboard
-    // For now, let's assume we will separate login page later
+    return <Component {...pageProps} />;
   }
 
-  // Check if the current page is the login/auth page
-  // In our current setup, the index page handles both auth and dashboard
-  // Let's wrap Component in Layout conditionally based on pageProps
-  const isAuthPage = !pageProps.user; // A simple way to check if it's the auth page
-
-  if (isAuthPage) {
-    return <Component {...pageProps} />; // Render component without layout
-  }
-
+  // For all other pages, use the layout.
   return (
     <Layout>
       <Component {...pageProps} />
